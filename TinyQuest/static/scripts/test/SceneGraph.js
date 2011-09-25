99a9dfb4b4a8559778478b82578dce1c2dd9dcf7
daigo <daigo.gamemaker@gmail.com>
2011-09-25 19:04:45 +0900

    Removed magic numbers

--- TinyQuest/static/scripts/test/SceneGraph.js
+++ TinyQuest/static/scripts/test/SceneGraph.js
@@ -1,26 +1,32 @@
 enchant();
 
-
-var game = new enchant.Game(320, 320);
+var GAME_WIDTH = 480;
+var GAME_HEIGHT = 320;
+var game = new enchant.Game(GAME_WIDTH, GAME_HEIGHT);
 enchant.loader.setRootPath('../../static/assets');
 var currentAnimationFileName = null;
 var currentAnimation = null;
+
+var target = new enchant.canvas.Node();
+target.position = [60, 160];
+target.update();
+
 var root = new enchant.canvas.Node();
 window.onload = function() {
-    game.fps = 45;
+    game.fps = 40;
 
     game.preload('../../static/assets/images/bg/bg0001.png');
     game.onload = function() {
         var stage = new Group();
 
-        var surface = new Surface(320,320);
+        var surface = new Surface(GAME_WIDTH,GAME_HEIGHT);
         var sceneGraph = new enchant.canvas.SceneGraph(game, surface);
         
-        root.position = [160, 160];
+        root.position = [GAME_WIDTH / 2, 160];
         root.scale = [2, 2];
         sceneGraph.setRoot(root);
 
-        var s = new enchant.Sprite(320,320);
+        var s = new enchant.Sprite(GAME_WIDTH,GAME_HEIGHT);
         s.image = surface
         stage.addChild(s);
         game.rootScene.addChild(stage);
@@ -30,18 +36,18 @@ window.onload = function() {
         this.addEventListener('enterframe', function() {
             
             surface.context.setTransform(1, 0, 0, 1, 0, 0);
-            surface.context.clearRect(0, 0,320, 320);
+            surface.context.clearRect(0, 0,GAME_WIDTH, GAME_HEIGHT);
             
             s.image.context.beginPath();
             s.image.context.lineWidth = 1.0;
-            s.image.context.moveTo(0, 159.5);
-            s.image.context.lineTo(320, 159.5);
+            s.image.context.moveTo(0, GAME_HEIGHT / 2 - 0.5);
+            s.image.context.lineTo(GAME_WIDTH, GAME_HEIGHT / 2 - 0.5);
             s.image.context.stroke();
 
             s.image.context.beginPath();
             s.image.context.lineWidth = 1.0;
-            s.image.context.moveTo(159.5, 0);
-            s.image.context.lineTo(159.5, 320);
+            s.image.context.moveTo(GAME_WIDTH / 2 - 0.5, 0);
+            s.image.context.lineTo(GAME_WIDTH / 2 - 0.5, GAME_HEIGHT);
             s.image.context.stroke();
 
             if (currentAnimationFileName) {
@@ -77,7 +83,7 @@ var playAnimation = function(id) {
         enchant.loader.load(files, function() {
             interval = null;
             root.removeAllChildren();
-            var animation = enchant.animation.animationManager.CreateAnimation(enchant.loader.get(currentAnimationFileName));
+            var animation = enchant.animation.animationManager.CreateAnimation(enchant.loader.get(currentAnimationFileName), false, null, target);
             enchant.animation.animationManager.start(animation); 
         });
     });
