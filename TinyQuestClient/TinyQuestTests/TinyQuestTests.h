56f83bc60d728b958eacf3b558da5a64cf89e114
sato.daigo <sato.daigo@52040-MAC.local>
2011-11-26 23:04:22 +0900

    iOS Client again

---
+++ TinyQuestClient/TinyQuestTests/TinyQuestTests.h
@@ -0,0 +1,13 @@
+//
+//  TinyQuestTests.h
+//  TinyQuestTests
+//
+//  Created by sato.daigo on 11/11/26.
+//  Copyright (c) 2011年 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface TinyQuestTests : SenTestCase
+
+@end
---
+++ TinyQuestClient/TinyQuestTests/TinyQuestTests.m
@@ -0,0 +1,32 @@
+//
+//  TinyQuestTests.m
+//  TinyQuestTests
+//
+//  Created by sato.daigo on 11/11/26.
+//  Copyright (c) 2011年 __MyCompanyName__. All rights reserved.
+//
+
+#import "TinyQuestTests.h"
+
+@implementation TinyQuestTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in TinyQuestTests");
+}
+
+@end
