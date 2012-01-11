38ad167b21114b20c07c1138a9c0f12b84aaac44
daigo <daigo.gamemaker@gmail.com>
2012-01-12 00:09:46 +0900

    Added sprites for demo purpose

--- Unity/Assets/Script/AnimationSpawner.cs
+++ Unity/Assets/Script/AnimationSpawner.cs
@@ -2,24 +2,34 @@ using UnityEngine;
 using System.Collections;
 
 public class AnimationSpawner : MonoBehaviour {
-	public Transform planePrefab;
+	public GameObject spawnPoint;
 	public Roga2dAnimationPlayer player;
 	public GameObject target;
-	Roga2dRoot root ;
+	Roga2dRoot root;
 	
 	void Awake () {
-		Application.targetFrameRate = 10;
+		Application.targetFrameRate = 60;
 	}
 	
 	// Use this for initialization
 	void Start () {
-		
-
 		this.root = new Roga2dRoot(player);
 		Roga2dNode target = new Roga2dNode(this.target);
 		target.Origin = target.Position;
 		this.root.Target = target;
 		
+		{
+		Roga2dRenderObject renderObject = new Roga2dRenderObject(Roga2dResourceManager.getTexture("bg/bg0001"), new Vector2(192, 256), new Vector2(0, 0), new Rect(0, 0, 128, 64));
+		Roga2dSprite sprite = new Roga2dSprite(renderObject);
+		sprite.GameObject.transform.parent = root.Transform;
+		}
+		
+		{
+		Roga2dRenderObject renderObject = new Roga2dRenderObject(Roga2dResourceManager.getTexture("death_wind"), new Vector2(96, 96), new Vector2(0, 0), new Rect(0, 0, 128, 128));
+		Roga2dSprite sprite = new Roga2dSprite(renderObject);
+		sprite.GameObject.transform.position = new Vector3(0.0f, -2.0f, 0.5f);
+		sprite.GameObject.transform.parent = root.Transform;
+		}
 	}
 
 	static bool done = false;
@@ -30,14 +40,14 @@ public class AnimationSpawner : MonoBehaviour {
 			//if(done){return;}
 			//string id = "Battle/Skills/Axe/Bash";
 			//string id = "Battle/Skills/Effect/Stab01";
-			string id = "Battle/Skills/Sword/LeaveDance";
+			//string id = "Battle/Skills/Sword/LeaveDance";
 			//string id = "Battle/Skills/Battler/TestSwordUpper";
 			//string id = "Battle/Skills/Effect/PlaneCutTest";
 			//string id = "Battle/Skills/Effect/PlaneCutBottom";
 			//string id = "Battle/Skills/Battler/SwordDash";
 			//string id = "Battle/Skills/Battler/SwordUpper";
 			//string id = "Battle/Skills/Laser/test";
-			//string id = "Battle/Skills/Laser/Skill_Laser01";
+			string id = "Battle/Skills/Laser/Skill_Laser01";
 			//string id = "Battle/Skills/Spear/SpearAirraid";
 			//string id = "Battle/Skills/Bow/Shoot";
 			//string id = "Battle/Skills/Bow/bow_bomb";
@@ -53,7 +63,7 @@ public class AnimationSpawner : MonoBehaviour {
 			//string id = "Battle/Skills/Fire/FlareParticle";
 			//string id = "Battle/Skills/Common/MagicCasting";
 			Roga2dAnimation animation = Roga2dUtils.LoadAnimation(id, false, 1.0f, 0.5f, this.root);
-			player.Play(player.transform, animation);
+			player.Play(this.spawnPoint.transform, animation);
 			done = true;
 		}
 	}
