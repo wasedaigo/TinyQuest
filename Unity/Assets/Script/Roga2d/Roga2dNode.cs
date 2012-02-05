9a3a9e4e888c3317437d969dd73e5400678b1d1d
daigo <daigo.gamemaker@gmail.com>
2012-02-06 00:38:25 +0900

    Skip movement/priority calculations if possible

--- Unity/Assets/Script/Roga2d/Roga2dNode.cs
+++ Unity/Assets/Script/Roga2d/Roga2dNode.cs
@@ -176,25 +176,29 @@ public class Roga2dNode {
 	public virtual void UpdateMovement() {
 		// Add velocity
 		// TODO: Should be remove in future. Use interval for this kind of move!
-		Vector2 velocity = this.Velocity;
-		Vector2 position = this.LocalPosition;
-		this.LocalPosition = new Vector2(Roga2dUtils.RoundPrecision(position.x + velocity.x), Roga2dUtils.RoundPrecision(position.y + velocity.y));
+		if (this.Velocity.x != 0 && this.Velocity.y != 0) {
+			Vector2 velocity = this.Velocity;
+			Vector2 position = this.LocalPosition;
+			this.LocalPosition = new Vector2(Roga2dUtils.RoundPrecision(position.x + velocity.x), Roga2dUtils.RoundPrecision(position.y + velocity.y));
+		}
 	}
 	
 	public virtual void UpdatePriority() {
+		float lastPriority = this.priority;
         if (this.Parent == null) {
             this.priority = this.LocalPriority;
         } else {
 			this.priority = 2 * this.LocalPriority * this.Parent.Priority;
         }
 		// Move z position of the node, so that it reflects its render-priority
-		// TODO: Only update if priority has changed
-		Transform transform = this.transform;
-		transform.position = new Vector3(
-			transform.position.x, 
-			transform.position.y, 
-			this.priority
-		);
+		if (lastPriority != this.priority) {
+			Transform transform = this.transform;
+			transform.position = new Vector3(
+				transform.position.x, 
+				transform.position.y, 
+				this.priority
+			);
+		}
 	}
 
 	public virtual void Update() {
