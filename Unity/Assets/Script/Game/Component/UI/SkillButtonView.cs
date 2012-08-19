dcd73407a3e1814feeb0a2b29619b08347a58937
daigo <daigo.gamemaker@gmail.com>
2012-08-20 01:06:49 +0900

    - Change Face direction for the opponent - Change remain HP color

--- Unity/Assets/Script/Game/Component/UI/SkillButtonView.cs
+++ Unity/Assets/Script/Game/Component/UI/SkillButtonView.cs
@@ -20,7 +20,7 @@ public class SkillButtonView : MonoBehaviour {
 		this.deadSign.SetActiveRecursively(false);
 	}
 
-	public void SetFaceIcon(int puppetId) {
+	public void SetFaceIcon(int puppetId, bool isOpponent) {
 		
 		if (this.faceIconNode != null) {
 			this.faceIconNode.Destroy();
@@ -31,7 +31,12 @@ public class SkillButtonView : MonoBehaviour {
 		actor.Transform.parent = faceIcon.transform;
 		actor.Transform.localPosition = new Vector3(0, 0, -0.1f);
 		actor.Transform.localEulerAngles = new Vector3(0, 0, 180);
-		actor.Transform.localScale = new Vector3(-24, 24, 0);
+		
+		if (isOpponent) {
+			actor.Transform.localScale = new Vector3(24, 24, 0);
+		} else {
+			actor.Transform.localScale = new Vector3(-24, 24, 0);
+		}
 		
 		Utils.SetLayerRecursively(actor.Transform, 5);
 		this.faceIconNode = actor;
@@ -51,11 +56,24 @@ public class SkillButtonView : MonoBehaviour {
 	}
 	
 	public void SetLife(int life, int maxLife) {
+		UISprite lifeBarImageSprite = lifeBarImage.GetComponent<UISprite>();
 		
 		if (life < this.life) {
 			iTween.ShakePosition(this.gameObject, iTween.Hash("x", 0.02f, "y", 0.02f,"time", 0.2f));
 		}
 		this.life = life;
+		
+		// change color depends on remains
+		float ratio = life / (float)maxLife;
+		if (ratio > 0.5f) {
+			lifeBarImageSprite.color = new Color(0.6f, 0.8f, 0.0f, 1.0f);
+		} else {
+			if (ratio > 0.25f) {
+				lifeBarImageSprite.color = new Color(1.0f, 0.8f, 0.2f, 1.0f);
+			} else {
+				lifeBarImageSprite.color = new Color(1.0f, 0.2f, 0.2f, 1.0f);	
+			}
+		}
 		
 		lifeBarImage.transform.localScale = new Vector3(life / (float)maxLife, 1, 1);
 		deadSign.SetActiveRecursively(IsDead());
@@ -64,7 +82,7 @@ public class SkillButtonView : MonoBehaviour {
 	public void UpdateStatus(CombatUnit combatUnit) {
 		this.SetLife(combatUnit.hp, combatUnit.GetUserUnit().MaxHP);
 		if (!initialized) {
-			this.SetFaceIcon(combatUnit.GetUserUnit().Unit.id);
+			this.SetFaceIcon(combatUnit.GetUserUnit().Unit.id, combatUnit.groupNo == 1);
 			this.initialized = true;
 		}
 	}
